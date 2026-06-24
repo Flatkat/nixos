c79bbfa97645e4b5a3533c9ad38ef2d71fa13740
flatkat <87091676+Flatkat@users.noreply.github.com>
2026-06-24 16:48:04 +0200

    Set up printer

--- systems/timber-hearth/configuration.nix
+++ systems/timber-hearth/configuration.nix
@@ -196,6 +196,21 @@
     ];
   };
 
+  services.avahi = {
+  enable = true;
+  nssmdns4 = true;
+  openFirewall = true;
+};
+
+services.printing = {
+  enable = true;
+  drivers = with pkgs; [
+    cups-filters
+    cups-browsed
+  ];
+};
+
+
   #systemd.user.services.vicinae = {
   #  Unit = {
   #    Description = "Vicinae server daemon";
